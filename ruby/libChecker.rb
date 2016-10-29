ed3ee225a5e0233c7a786476d40c56d489fdeac8
ifriedman08 <i.friedman.08@gmail.com>
2016-10-28 18:13:29 -0700

    init commit

---
+++ ruby/libChecker.rb
@@ -0,0 +1,26 @@
+require "json"
+
+puts "Starting libChecker"
+
+system "pwd"
+# puts Dir["lib/*.mp3"]
+
+dataArray = []
+
+Dir["lib/*.mp3"].each do |file|
+  path = file
+  name = file.split('/')[1].gsub('.mp3', '')
+  dataArray.push({
+    name: name,
+    path: file
+  })
+end
+
+puts dataArray.to_json
+
+File.open("json/soundFiles.json","w") do |f|
+  f.write(dataArray.to_json)
+end
+
+# puts Dir["lib/*.mp3"].class
+# puts Dir["lib/*.mp3"].length
