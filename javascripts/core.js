71cce1a67cbc9ffc8371080b771f0ac6b6ff8e0c
ifriedman08 <i.friedman.08@gmail.com>
2016-10-28 23:47:13 -0700

    added timeout to callback

--- javascripts/core.js
+++ javascripts/core.js
@@ -2,12 +2,15 @@ window.dnd_sb = {};
 
 $.getJSON('json/soundFiles.json', function(data){
   dnd_sb.mp3Json = data;
-  dnd_sb.mp3Json.forEach(function (obj) {
-    var soundDiv = $(`<div>
-      <h6>` + obj.name + `</h6>
-      <audio controls>
-      <source src=` + obj.path + ` type="audio/mp3">
-      </audio></div>`);
+  setTimeout(function () {
+    dnd_sb.mp3Json.forEach(function (obj) {
+      var soundDiv = $(`<div>
+        <h6>` + obj.name + `</h6>
+        <audio controls>
+        <source src=` + obj.path + ` type="audio/mp3">
+        </audio></div>`
+      );
       $('body').append(soundDiv);
     })
+  }, 5000)
 });
