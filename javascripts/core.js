2acfd9c826652c35699fead7a57c420359a4b5c4
ifriedman08 <i.friedman.08@gmail.com>
2016-10-28 18:42:34 -0700

    minor syntax fix

--- javascripts/core.js
+++ javascripts/core.js
@@ -3,9 +3,9 @@ window.dnd_sb = {};
 $.getJSON('json/soundFiles.jsonp', function(data){dnd_sb.mp3Json = data});
 
 dnd_sb.mp3Json.forEach(function (obj) {
-  var soundDiv = $("<div><audio controls>
+  var soundDiv = $(`<div><audio controls>
     <label>" + obj.name + "</label>
     <source src=" + obj.path + " type="audio/mp3">
-  </audio></div>");
+  </audio></div>`);
   $('body').append(soundDiv);
 })
