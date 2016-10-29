f19fe645c007b5dd65313b9e2fc5e8aa33059305
ifriedman08 <i.friedman.08@gmail.com>
2016-10-28 18:47:12 -0700

    moved array process to jq callback

--- javascripts/core.js
+++ javascripts/core.js
@@ -1,11 +1,12 @@
 window.dnd_sb = {};
 
-$.getJSON('json/soundFiles.jsonp', function(data){dnd_sb.mp3Json = data});
-
-dnd_sb.mp3Json.forEach(function (obj) {
-  var soundDiv = $(`<div><audio controls>
-    <label>` + obj.name + `</label>
-    <source src=` + obj.path + ` type="audio/mp3">
-  </audio></div>`);
-  $('body').append(soundDiv);
-})
+$.getJSON('json/soundFiles.jsonp', function(data){
+  dnd_sb.mp3Json = data;
+  dnd_sb.mp3Json.forEach(function (obj) {
+    var soundDiv = $(`<div><audio controls>
+      <label>` + obj.name + `</label>
+      <source src=` + obj.path + ` type="audio/mp3">
+      </audio></div>`);
+      $('body').append(soundDiv);
+    })
+});
