ba2a28464c543b4d27d3b53ef4f11700fdbc7c83
ifriedman08 <i.friedman.08@gmail.com>
2016-10-28 18:50:47 -0700

    changed label to h6

--- javascripts/core.js
+++ javascripts/core.js
@@ -3,8 +3,9 @@ window.dnd_sb = {};
 $.getJSON('json/soundFiles.jsonp', function(data){
   dnd_sb.mp3Json = data;
   dnd_sb.mp3Json.forEach(function (obj) {
-    var soundDiv = $(`<div><audio controls>
-      <label>` + obj.name + `</label>
+    var soundDiv = $(`<div>
+      <h6>` + obj.name + `</h6>
+      <audio controls>
       <source src=` + obj.path + ` type="audio/mp3">
       </audio></div>`);
       $('body').append(soundDiv);
