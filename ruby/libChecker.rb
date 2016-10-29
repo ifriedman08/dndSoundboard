188c40fb45ee0a1c264f1c321fac11f3b8d05cbb
ifriedman08 <i.friedman.08@gmail.com>
2016-10-28 23:42:51 -0700

    added xml

--- ruby/libChecker.rb
+++ ruby/libChecker.rb
@@ -24,5 +24,18 @@ File.open("json/soundFiles.json","w") do |f|
   f.write(dataArray.to_json)
 end
 
+f = File.new("xml/soundFiles.xml", "w")
+Dir["lib/*.mp3"].each do |file|
+  f.write("<soundfile>\n")
+  path = file
+  system ("mv " + file.shellescape + " " + file.gsub(" ", "_"))
+  name = file.split('/')[1].gsub('.mp3', '')
+
+  f.write("\t<name>" + name + "</name>\n")
+  f.write("\t<path>" + path + "</path>\n")
+  f.write("</soundfile>\n\n")
+end
+f.close
+
 # puts Dir["lib/*.mp3"].class
 # puts Dir["lib/*.mp3"].length
