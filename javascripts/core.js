0de2a185fa6a93f710b25430ba566d77d7b658ce
ifriedman08 <i.friedman.08@gmail.com>
2016-10-28 23:56:06 -0700

    only buttons visible

--- javascripts/core.js
+++ javascripts/core.js
@@ -3,14 +3,19 @@ window.dnd_sb = {};
 $.getJSON('json/soundFiles.json', function(data){
   dnd_sb.mp3Json = data;
   setTimeout(function () {
-    dnd_sb.mp3Json.forEach(function (obj) {
-      var soundDiv = $(`<div>
+    dnd_sb.mp3Json.forEach(function (obj, idx) {
+      var audioDiv = $(`<div id="`+ idx +`" style="display:none">
         <h6>` + obj.name + `</h6>
         <audio controls>
         <source src=` + obj.path + ` type="audio/mp3">
         </audio></div>`
       );
-      $('body').append(soundDiv);
+      var button = $('<button class="audio_trigger" id="' + idx + '">' + obj.name + '</button>');
+      $('body').append(audioDiv);
+      $('body').append(button);
+    });
+    $('button.audio_trigger').click(function(){
+      console.log(this);
     })
   }, 5000)
 });
